e33172ac16a6a07bed052be1b430b4687e8d2aa2
Huu Hung <hungmnh01@gmail.com>
2024-05-13 14:11:36 +0700

    video 61

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -2,10 +2,20 @@ package vn.hoidanit.laptopshop.service;
 
 import org.springframework.stereotype.Service;
 
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
+
 @Service
 public class UserService {
+    private final UserRepository userRepository;
 
-    public String handleHello() {
-        return "hello from service";
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User handleSaveUser(User user) {
+        User eric = this.userRepository.save(user);
+        System.out.println(eric);
+        return eric;
     }
 }
